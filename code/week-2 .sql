798aac9e1514fc31acf1786990f16109041c0c17
dnwlse <164366626+dnwlse@users.noreply.github.com>
2024-09-10 12:48:06 +0900

    20211864 장우진

---
+++ code/week-2 .sql
@@ -0,0 +1,17 @@
+CREATE DATABASE dju24;
+
+CREATE TABLE teachers(
+	id bigserial,
+	first_name varchar(25),
+	last_name varchar(50),
+	school varchar(50),
+	hire_date date,
+	salary numeric
+);
+
+INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
+VALUES ('janet', 'smith', 'F.D.Rooservelt H.S.', '2011-10-30', 36200),
+		('Lee', 'Reynolds', 'F.D.Rooservelt H.S.', '1993-05-22', 65000),
+		('samuel', 'Cole', 'Meyers M.S.', '2005-08-01', 43500);
+
+SELECT * from teachers;
